0d2b749ffea970359f4f3e1617651c4872fc1480
LOU Chumdararith <louchumdararith02@gmail.com>
2025-10-18 09:58:08 +0700

    My first e-commerce project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    This is my first VueJs project
+  </div>
+</template>
+
+<style scoped>
+
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
+
+</style>
