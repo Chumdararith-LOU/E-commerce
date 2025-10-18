a09928d9a3653a765784f996120df726fd7caf7b
LOU Chumdararith <louchumdararith02@gmail.com>
2025-10-18 10:11:15 +0700

    Change to npm

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div>
-    This is my first VueJs project
-  </div>
+  <div>This is my first VueJs project</div>
 </template>
 
 <style scoped>
-
-div {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-}
 
 </style>
